07da780bf3dbe9d5b4f05d4bdf9ddfaa8f654801
davisoll <benifei.davi@gmail.com>
2023-05-12 17:39:11 -0300

    [configuração] Criei os componentes principais do projeto

---
+++ ecommerce-sol/src/Componentes/ShoppingCart/Cart/Cart.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Itens } from "../Itens/Itens";
+
+export function Cart () {
+
+    return (
+        <div>
+            <p>Cart</p>
+            <Itens />
+        </div>
+    )
+}
---
+++ ecommerce-sol/src/Componentes/ShoppingCart/Itens/Itens.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export function Itens () {
+
+    return (
+        <p>Itens</p>
+    )
+}
